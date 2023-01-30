852c56910be88590d8689607f85009d72fcc6848
cristi <scarlat.cristian.gabriel@gmail.com>
2023-01-30 16:13:37 +0200

    finish design

---
+++ app.js
@@ -0,0 +1,28 @@
+"use strict";
+const nav = document.querySelector(".nav");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnOpenModal = document.querySelectorAll(".btn--open-modal");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnScrollTo = document.querySelector("btn--scroll-to");
+
+// Open modal window
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) closeModal();
+});
